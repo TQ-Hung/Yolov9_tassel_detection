949fbe36e55c65a3dd37cc694ba4ebe8e06c6b9a
TQ-Hung <133394852+TQ-Hung@users.noreply.github.com>
2024-04-11 17:00:57 +0700

    Update common.py

--- common.py
+++ common.py
@@ -1213,14 +1213,29 @@ class Classify(nn.Module):
         return self.linear(self.drop(self.pool(self.conv(x)).flatten(1)))
 
 
-class SimAM(nn.Module):
-    def __init__(self, lambda_coeff=0.5):  # Thêm tham số lambda_coeff với giá trị mặc định
-        super(SimAM, self).__init__()
-        self.lambda_coeff = lambda_coeff  # Lưu giá trị lambda_coeff
+class simam_module(torch.nn.Module):
+    def __init__(self, channels = None, e_lambda = 1e-4):
+        super(simam_module, self).__init__()
 
-    def forward(self, X):
-        n = X.shape[2] * X.shape[3] - 1
-        d = (X - X.mean(dim=[2, 3])).pow(2)
-        v = d.sum(dim=[2, 3]) / n
-        E_inv = d / (4 * (v + self.lambda_coeff)) + 0.5
-        return X * torch.sigmoid(E_inv)
+        self.activaton = nn.Sigmoid()
+        self.e_lambda = e_lambda
+
+    def __repr__(self):
+        s = self.__class__.__name__ + '('
+        s += ('lambda=%f)' % self.e_lambda)
+        return s
+
+    @staticmethod
+    def get_module_name():
+        return "simam"
+
+    def forward(self, x):
+
+        b, c, h, w = x.size()
+        
+        n = w * h - 1
+
+        x_minus_mu_square = (x - x.mean(dim=[2,3], keepdim=True)).pow(2)
+        y = x_minus_mu_square / (4 * (x_minus_mu_square.sum(dim=[2,3], keepdim=True) / n + self.e_lambda)) + 0.5
+
+        return x * self.activaton(y)
