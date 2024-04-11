f7ae1aad54674aff4463b391441ae72de223dc7d
TQ-Hung <133394852+TQ-Hung@users.noreply.github.com>
2024-04-11 21:27:00 +0700

    Update common.py

--- common.py
+++ common.py
@@ -1240,24 +1240,27 @@ class simam_module(torch.nn.Module):
 
         return x * self.activaton(y)
 class GSConv(nn.Module):
-    def __init__(self, c1, c2, k=1, s=1, g=1, act=True):
+    # GSConv https://github.com/AlanLi1997/slim-neck-by-gsconv
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
         super().__init__()
         c_ = c2 // 2
-        self.cv1 = Conv(c1, c_, k, s, None, g, act)
-        self.cv2 = Conv(c_, c_, 5, 1, None, c_, act)
+        
+        # Sử dụng định nghĩa của class Conv
+        self.cv1 = Conv(c1, c_, k, s, p, g, d, act)
+        self.cv2 = Conv(c_, c_, 5, 1, p, c_, d, act)
 
     def forward(self, x):
+        # Forward qua cv1 và cv2
         x1 = self.cv1(x)
-        x2 = torch.cat((x1, self.cv2(x1)), 1)
-        # shuffle
-        # y = x2.reshape(x2.shape[0], 2, x2.shape[1] // 2, x2.shape[2], x2.shape[3])
-        # y = y.permute(0, 2, 1, 3, 4)
-        # return y.reshape(y.shape[0], -1, y.shape[3], y.shape[4])
-
+        x2 = torch.cat((x1, self.cv2(x1)), dim=1)
+        
+        # Shuffle layer
         b, n, h, w = x2.data.size()
         b_n = b * n // 2
         y = x2.reshape(b_n, 2, h * w)
         y = y.permute(1, 0, 2)
-        y = y.reshape(2, -1, n // 2, h, w)
+        y = y.reshape(2, b, n // 2, h, w)
+        
+        # Hoán đổi vị trí của y và nối các phần lại với nhau
+        return torch.cat((y[0], y[1]), dim=1)
 
-        return torch.cat((y[0], y[1]), 1)
